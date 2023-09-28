3d2cedc4fbcbc0ad07c6970deb4b58bb40d403be
Deeraj <sdeeraj2000@gmail.com>
2023-09-28 10:24:49 +0530

    made changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css';
 function App() {
   return (
 
-    <BrowserRouter basename='/testlog'>
+    <BrowserRouter>
       <Routes>
         <Route path='/signup' element={<Signup />}></Route>
         <Route path='/signin' element={<Signin />}></Route>
